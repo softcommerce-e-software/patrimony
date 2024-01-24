37089bd4ea51f17027748b6daf8f756c2f438a82
Lucas Eduardo <lucaseduardocrp@gmail.com>
2024-01-24 16:59:28 -0300

    feat: created custom buttons

--- lib/uikit/components/buttons/custom_image_button.dart
+++ lib/uikit/components/buttons/custom_image_button.dart
@@ -26,43 +26,40 @@ class CustomImageButton extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Material(
-      borderRadius: _borderRadius,
-      child: Ink(
-        height: 52.0,
-        width: double.infinity,
-        decoration: BoxDecoration(
-          color: isDisable ? Theme.of(context).disabledColor : background,
-          borderRadius: _borderRadius,
-          boxShadow: [
-            BoxShadow(
-              color: Theme.of(context).primaryColorDark.withOpacity(0.3),
-              spreadRadius: 0,
-              blurRadius: 4,
-              offset: const Offset(0, 2),
+    return Ink(
+      height: 52.0,
+      width: double.infinity,
+      decoration: BoxDecoration(
+        color: isDisable ? Theme.of(context).disabledColor : background,
+        borderRadius: _borderRadius,
+        boxShadow: [
+          BoxShadow(
+            color: Theme.of(context).primaryColorDark.withOpacity(0.3),
+            spreadRadius: 0,
+            blurRadius: 4,
+            offset: const Offset(0, 2),
+          ),
+        ],
+      ),
+      child: InkWell(
+        borderRadius: _borderRadius,
+        onTap: onPressed,
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            SizedBox(
+              height: 16.0,
+              child: Image.asset(iconPath),
+            ),
+            const SizedBox(width: 12.0),
+            Text(
+              buttonText.toUpperCase(),
+              style: Theme.of(context).textTheme.titleSmall?.apply(
+                    color: textColor,
+                  ),
             ),
           ],
-        ),
-        child: InkWell(
-          borderRadius: _borderRadius,
-          onTap: onPressed,
-          child: Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: [
-              SizedBox(
-                height: 16.0,
-                child: Image.asset(iconPath),
-              ),
-              const SizedBox(width: 12.0),
-              Text(
-                buttonText.toUpperCase(),
-                style: Theme.of(context).textTheme.titleSmall?.apply(
-                      color: textColor,
-                    ),
-              ),
-            ],
-          ),
         ),
       ),
     );
